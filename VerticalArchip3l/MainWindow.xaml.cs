e77db3ccd1b1c01652e735451a064c77d69efd0d
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-01-23 20:14:18 +0100

    Print medals and timer
    Basic timer working

--- VerticalArchip3l/MainWindow.xaml.cs
+++ VerticalArchip3l/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace VerticalArchip3l
 {
@@ -21,10 +22,39 @@ namespace VerticalArchip3l
     /// </summary>
     public partial class MainWindow : SofthinkWindow
     {
+        DateTime remainingTime;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            //Managing time
+            remainingTime = new DateTime(1, 1, 1, 0, 0, 15);
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            string minutes, secondes;
+            if (remainingTime.Minute != 0 || remainingTime.Second != 0)
+            {
+                remainingTime = remainingTime.AddSeconds(-1);
+                if (remainingTime.Minute > 9)
+                    minutes = remainingTime.Minute.ToString();
+                else
+                    minutes = "0" + remainingTime.Minute.ToString();
+                if (remainingTime.Second > 9)
+                    secondes = remainingTime.Second.ToString();
+                else
+                    secondes = "0" + remainingTime.Second.ToString();
+                TextBlockRemainingTime.Content = "Temps restant : " + minutes + ":" + secondes;
+            }
+            else
+            {
+                TextBlockRemainingTime.Foreground = Brushes.Red;
+            }
         }
     }
 }
