2623948e4cbc4330dfaf2dde6a5e69ee9264fd84
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-01-25 13:05:47 +0100

    Trophy class, partial bonus class, trophy displays

---
+++ VerticalArchip3l/Bonus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerticalArchip3l
+{
+    class Bonus
+    {
+        private string Name;
+
+        public Bonus(string name)
+        {
+            this.Name = name;
+
+        }
+        public bool applyBonus()
+        {
+            return true;
+        }
+    }
+}
